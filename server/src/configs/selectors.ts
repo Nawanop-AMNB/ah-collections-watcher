82835df70f60f5788c2a3c3563ce235e786bd38d
Nawanop <nawanop@convolab.ai>
2022-03-30 23:57:34 +0700

    update url regex and fix seletor

--- server/src/configs/selectors.ts
+++ server/src/configs/selectors.ts
@@ -4,10 +4,8 @@ const EXPLORE_BUTTON =
   "#root > div:nth-child(4) > div > div.d-block.d-md-flex > div.dropdown-block.dropdown-primary.flex-wrapper.dropdown > button";
 const SORT_BY_CREATED_AT_BUTTON =
   "#root > div:nth-child(4) > div > div.d-block.d-md-flex > div.dropdown-block.dropdown-primary.flex-wrapper.show.dropdown > div > a:nth-child(4)";
-const COLLECTION_CARD =
-  "#root > div:nth-child(4) > div > div.row.mb-4 > div > div";
 const WHITELIST_CHECKBOX =
-  "#root > div:nth-child(4) > div > div.d-block.d-md-flex > div.CustomCheckbox.checkbox-control.mx-auto.d-flex.flex-wrapper.mb-2 > input";
+  ".d-block > .CustomCheckbox > .square > .svg-inline--fa > path";
 const NEXT_BUTTON =
   ".container > .mt-4 > .ApiPagination > .pagination-main > .pagination-button:nth-child(3)";
 
@@ -15,7 +13,6 @@ export const AH_DETECTOR = {
   ACCEPT_COOKIE_BUTTON,
   EXPLORE_BUTTON,
   SORT_BY_CREATED_AT_BUTTON,
-  COLLECTION_CARD,
   WHITELIST_CHECKBOX,
   NEXT_BUTTON,
 };
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -73,8 +73,7 @@ export function getDiscordInviteRegex() {
 }
 
 export function getUrlRegex() {
-  const regex =
-    "((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)";
+  const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/gi;
   return new RegExp(regex, "gi");
 }
 
